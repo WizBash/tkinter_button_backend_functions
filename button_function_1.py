fd056c03be6d9cc5caed8edf67877a3cb8609708
WizBash <53521065+WizBash@users.noreply.github.com>
2021-01-16 00:56:48 -0800

    Update button_function_1.py

--- button_function_1.py
+++ button_function_1.py
@@ -1,12 +1,16 @@
 from tkinter import*
 
-
+def press():
+  label.config(text='button clicked')
 
 root=Tk()
 root.title('button_functions')
 
-Btn = Button(width=10,text='Click Me')
-Btn.pack()
+
+Btn = Button(width=10,text='Click Me',command=press)
+Btn.grid(row=1, column=1)
+label=Label(text='not clicked')
+label.grid(row=2,column=1)
 
 
 
