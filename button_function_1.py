a57dbb12fe38dca6e1899a05342f1a72defee6d4
WizBash <53521065+WizBash@users.noreply.github.com>
2021-01-17 01:58:51 -0800

    Update button_function_1.py

--- button_function_1.py
+++ button_function_1.py
@@ -2,16 +2,21 @@ from tkinter import*
 
 #============vutton function=================
 def press():
-  label.config(text='button clicked')
+  label.config(text='button 1 clicked')
+def press2():
+  label.config(text='button 2 clicked')
 
 root=Tk()
 root.title('button_functions')
 
 
-Btn = Button(width=10,text='Click Me',command=press)
+Btn = Button(width=10,text='button 1',command=press)
 Btn.grid(row=1, column=1)
 label=Label(text='not clicked')
 label.grid(row=2,column=1)
+
+btn2=Button(width=10,text='Button 2',command=press2)
+Btn.grid(row=3, column=1)
 
 
 
